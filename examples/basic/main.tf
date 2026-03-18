440555d5216680e85282817de6150e7902ee8159
Craig Sloggett <craigsloggett@gmail.com>
2026-03-17 20:00:22 -0400

    docs: Update README

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,10 +1,15 @@
-data "aws_ami" "hc_base" {
+data "aws_ami" "debian" {
   most_recent = true
-  owners      = ["888995627335"]
+  owners      = ["136693071363"]
 
   filter {
     name   = "name"
-    values = ["hc-base-ubuntu-2404-amd64-*"]
+    values = ["debian-13-amd64-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 }
 
@@ -15,5 +20,5 @@ module "vault" {
   route53_zone_name   = var.route53_zone_name
   vault_license       = var.vault_license
   ec2_key_pair_name   = var.ec2_key_pair_name
-  ec2_instance_ami_id = data.aws_ami.hc_base.id
+  ec2_instance_ami_id = data.aws_ami.debian.id
 }
