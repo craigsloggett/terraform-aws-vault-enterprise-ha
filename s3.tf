bf6767b42b3aff4668fb815a915a6fbe1e1dd71e
Craig Sloggett <craigsloggett@gmail.com>
2026-03-20 15:10:16 -0400

    fix: Add bucket_namespace for account-regional S3 naming

--- s3.tf
+++ s3.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "vault_snapshots" {
-  bucket = "${var.project_name}-vault-snapshots-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.region}-an"
+  bucket           = "${var.project_name}-vault-snapshots-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.region}-an"
+  bucket_namespace = "account-regional"
 
   tags = merge(var.common_tags, { Name = "${var.project_name}-vault-snapshots" })
 }
