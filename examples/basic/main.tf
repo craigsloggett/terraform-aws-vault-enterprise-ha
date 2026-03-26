9080c04e633e2c72694980a70c081371ddb8b4b4
Craig Sloggett <craigsloggett@gmail.com>
2026-03-26 14:08:22 -0400

    Revert source after testing

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -14,8 +14,7 @@ data "aws_ami" "selected" {
 
 module "vault" {
   # tflint-ignore: terraform_module_pinned_source
-  #source = "git::https://github.com/craigsloggett/terraform-aws-vault-enterprise"
-  source = "../../"
+  source = "git::https://github.com/craigsloggett/terraform-aws-vault-enterprise"
 
   project_name      = var.project_name
   route53_zone      = data.aws_route53_zone.selected
